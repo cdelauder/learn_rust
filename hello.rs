f4c951418ffcbc1437b2a86cdc697ce8af335213
cdelauder <cdelauder@gmail.com>
2015-05-10 18:26:12 -0700

    my program can take command line input. I have reverted to a dbc newb

--- hello.rs
+++ hello.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main() {
   println!("Step right up, guess the number");
   let mut guess = String::new();
-  io::stdin().read_line(&mut guess)
+  io::stdin().readline(&mut guess)
   .ok()
   .expect("failed to get the input");
   println!("Your guess was {}", guess);
