4c544408049cc7a6e43c41a36515c144eedb034c
cdelauder <cdelauder@gmail.com>
2015-05-10 18:47:23 -0700

    finished the guess a number game. Starting to get some of the broader ideas

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
   println!("Step right up, guess the number");
-  let mut guess = String::new();
-  io::stdin().read_line(&mut guess)
-  .ok()
-  .expect("failed to get the input");
-  println!("Your guess was {}", guess);
+  let secret_num = rand::thread_rng().gen_range(1, 101);
+  loop {
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+      .ok()
+      .expect("failed to get the input");
+    println!("Your guess was {}", guess);
+
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("let's keep it a number you putz");
+        continue;
+      }
+    };
+
+    match guess.cmp(&secret_num) {
+      Ordering::Less    => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal   => {
+        println!("You win!");
+        break;
+      }
+    }
+  }
 }
